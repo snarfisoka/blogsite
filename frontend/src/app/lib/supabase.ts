281d0e575e59399df6a1f7f08e4367302947a83e
Frans <77536059+snarfisoka@users.noreply.github.com>
2025-08-02 03:03:36 +0800

    Delete frontend directory

--- frontend/src/app/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://supabase.com/dashboard/project/rmqpfqtcbzmylxmppehi';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtcXBmcXRjYnpteWx4bXBwZWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MDAzNDcsImV4cCI6MjA2OTA3NjM0N30.LfYwttVguSZIu4ypj5U_UP8vNo6FbbcElXmUy_b0IXU';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
